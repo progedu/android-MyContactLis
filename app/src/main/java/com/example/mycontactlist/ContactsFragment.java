15e6f9102d7f50f87ea5a2f316a9e7963ed758a3
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-12-06 17:22:07 +0900

    Add Fragment layout

--- app/src/main/java/com/example/mycontactlist/ContactsFragment.java
+++ app/src/main/java/com/example/mycontactlist/ContactsFragment.java
@@ -1,17 +1,31 @@
 package com.example.mycontactlist;
 
-import android.app.LoaderManager;
-import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
-
 
 public class ContactsFragment extends Fragment implements
         LoaderManager.LoaderCallbacks<Cursor>,
         AdapterView.OnItemClickListener {
+
+    // Empty public constructor, required by the system
+    public ContactsFragment() {
+    }
+
+    // A UI Fragment must inflate its View
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the fragment layout
+        return inflater.inflate(R.layout.contacts_list_view,
+                container, false);
+    }
 
     @Override
     public Loader<Cursor> onCreateLoader(int loaderId, Bundle args) {
